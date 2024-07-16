3f7a6ef234d4503e9cf74c97bd205a42e7857b6b
priyanshu kumar <141728977+priyanshu1520@users.noreply.github.com>
2024-07-16 22:15:21 +0530

    Time: 658 ms (8.22%), Space: 82.4 MB (32.19%) - LeetHub

--- 2217-step-by-step-directions-from-a-binary-tree-node-to-another/2217-step-by-step-directions-from-a-binary-tree-node-to-another.java
+++ 2217-step-by-step-directions-from-a-binary-tree-node-to-another/2217-step-by-step-directions-from-a-binary-tree-node-to-another.java
@@ -1,43 +1,58 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
 class Solution {
-    public TreeNode lowestCommonAncestor(TreeNode root, int p, int q) {
+    public TreeNode ancestor(TreeNode root,int p,int q){
         if(root==null){
             return null;
         }
         if(root.val==p||root.val==q){
             return root;
         }
-        TreeNode lca=lowestCommonAncestor(root.left,p,q);
-        TreeNode rca=lowestCommonAncestor(root.right,p,q);
+        TreeNode lca=ancestor(root.left,p,q);
+        TreeNode rca=ancestor(root.right,p,q);
         if(lca!=null&&rca!=null){
             return root;
         }
-        if(lca!=null){
+        else if(lca!=null){
             return lca;
         }else{
             return rca;
         }
     }
-    public boolean solve(TreeNode root,int target,ArrayList<Character> str){
+    public boolean solve(TreeNode root,int target,ArrayList<Character> ans){
         if(root==null){
             return false;
         }
         if(root.val==target){
             return true;
         }
-        str.add('L');
-        if(solve(root.left,target,str)){
+        ans.add('L');
+        if(solve(root.left,target,ans)){
             return true;
         }
-        str.remove(str.size()-1);
-        str.add('R');
-        if(solve(root.right,target,str)){
+        ans.remove(ans.size()-1);
+        ans.add('R');
+        if(solve(root.right,target,ans)){
             return true;
         }
-        str.remove(str.size()-1);
+        ans.remove(ans.size()-1);
         return false;
     }
     public String getDirections(TreeNode root, int startValue, int destValue) {
-        TreeNode node=lowestCommonAncestor(root,startValue,destValue);
+        TreeNode node=ancestor(root,startValue,destValue);
         ArrayList<Character> start=new ArrayList<>();
         ArrayList<Character> end=new ArrayList<>();
         solve(node,startValue,start);
