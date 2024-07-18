69fb46f8b5d28ab11cb5f5d35f98d16aa3c320df
priyanshu kumar <141728977+priyanshu1520@users.noreply.github.com>
2024-07-18 23:41:59 +0530

    Time: 7 ms (63.05%), Space: 45.3 MB (6.95%) - LeetHub

---
+++ 1653-number-of-good-leaf-nodes-pairs/1653-number-of-good-leaf-nodes-pairs.java
@@ -0,0 +1,53 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    int count=0;
+    public List<Integer> solve(TreeNode root,int d){
+        if(root==null){
+            return new ArrayList<>();
+        }
+        if(root.left==null&&root.right==null){
+            List<Integer> leaf=new ArrayList<>();
+            leaf.add(1);
+            return leaf;
+        }
+        List<Integer> l=solve(root.left,d);
+        List<Integer> r=solve(root.right,d);
+        for(int a:l){
+            for(int b:r){
+                if((a+b)<=d){
+                    count++;
+                }
+            }
+        }
+        List<Integer> ans=new ArrayList<>();
+        for(int a:l){
+            if(a+1<d){
+                ans.add(a+1);
+            }
+        }
+        for(int b:r){
+            if(b+1<d){
+                ans.add(b+1);
+            }
+        }
+        return ans;
+    }
+    public int countPairs(TreeNode root, int distance) {
+        solve(root,distance);
+        return count;
+    }
+}
