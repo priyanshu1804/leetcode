682f0fe2a7766f51c385c35fb791420a7f9b7e6b
priyanshu kumar <141728977+priyanshu1804@users.noreply.github.com>
2024-09-13 20:51:57 +0530

    Time: 2 ms (100%), Space: 54.9 MB (99.27%) - LeetHub

---
+++ 1435-xor-queries-of-a-subarray/1435-xor-queries-of-a-subarray.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int[] xorQueries(int[] arr, int[][] queries) {
+        int n=arr.length;
+        int[] pre=new int[n+1];
+        for(int i=0;i<n;i++){
+            pre[i+1]=pre[i]^arr[i];
+        }
+        int[] ans=new int[queries.length];
+        for(int i=0;i<queries.length;i++){
+            ans[i]=pre[queries[i][1]+1]^pre[queries[i][0]];
+        }
+        return ans;
+    }
+}
