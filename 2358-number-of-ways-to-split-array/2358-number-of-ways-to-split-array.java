c47677b6b7928f918cddbc69c55b11b89e8cc669
priyanshu kumar <141728977+priyanshu1804@users.noreply.github.com>
2025-01-03 13:30:12 +0530

    Time: 3 ms (59.69%), Space: 61.4 MB (7.38%) - LeetHub

--- 2358-number-of-ways-to-split-array/2358-number-of-ways-to-split-array.java
+++ 2358-number-of-ways-to-split-array/2358-number-of-ways-to-split-array.java
@@ -1,24 +1,14 @@
-class Solution {
-    public int waysToSplitArray(int[] nums) {
-        long[] pre=new long[nums.length];
-        long[] suff=new long[nums.length];
-        for(int i=0;i<nums.length;i++){
-            if(i==0){
-                pre[i]=nums[i];
-            }else{
-                pre[i]=pre[i-1]+nums[i];
-            }
-        }
-        for(int i=nums.length-1;i>=0;i--){
-            if(i==nums.length-1){
-                suff[i]=nums[i];
-            }else{
-                suff[i]=suff[i+1]+nums[i];
-            }
+class Solution{
+    public int waysToSplitArray(int[] nums){
+        int n=nums.length;
+        long[] pre=new long[n];
+        pre[0]=(long)nums[0];
+        for(int i=1;i<n;i++){
+            pre[i]=pre[i-1]+(long)nums[i];
         }
         int count=0;
-        for(int i=0;i<pre.length-1;i++){
-            if(pre[i]>=suff[i+1]){
+        for(int i=0;i<n-1;i++){
+            if(pre[i]>=pre[n-1]-pre[i]){
                 count++;
             }
         }
