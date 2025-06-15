dc10120e2c45808982bf7707ec8f614abfe356a6
priyanshu kumar <141728977+priyanshu1804@users.noreply.github.com>
2025-06-15 23:08:27 +0530

    Time: 1 ms (71.6%), Space: 41.5 MB (27.78%) - LeetHub

--- 1515-find-the-minimum-number-of-fibonacci-numbers-whose-sum-is-k/1515-find-the-minimum-number-of-fibonacci-numbers-whose-sum-is-k.java
+++ 1515-find-the-minimum-number-of-fibonacci-numbers-whose-sum-is-k/1515-find-the-minimum-number-of-fibonacci-numbers-whose-sum-is-k.java
@@ -1,16 +1,16 @@
 class Solution {
     public int findMinFibonacciNumbers(int k) {
-        long[] dp=new long[301];
+        long[] dp=new long[101];
         dp[0]=1;
         dp[1]=1;
-        for(int i=2;i<=300;i++){
+        for(int i=2;i<=100;i++){
             dp[i]=dp[i-1]+dp[i-2];
         }
-        System.out.println(dp[300]);
+        //System.out.println(dp[300]);
         int ans=0;
         while(k>0){
             int low=0;
-            int high=300;
+            int high=100;
             long ans1=0;
             while(low<=high){
                 int mid=low+(high-low)/2;
